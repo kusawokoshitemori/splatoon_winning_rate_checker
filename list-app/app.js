d0a9e59f950427f00ce3b8d5a54ce95546b87032
草をこして森 <kyou0317kyou0317@gmail.com>
2024-08-21 22:15:30 +0900

    UserNameを入れさせる初回のみ

--- list-app/app.js
+++ list-app/app.js
@@ -1,12 +1,26 @@
 // 必要なモジュールを読み込む
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
 
 // Expressアプリケーションを作成
 const app = express();
 
 // ポート番号の設定（環境変数から取得することが推奨されますが、デフォルトで3000を使用）
 const port = process.env.PORT || 3000;
+
+// 生成した秘密鍵をここに設定
+const secretKey =
+  "5e884898da28047151d0e56f8dc6294773603d0d9d80a3ad6a248f2e1c8e9c7a";
+
+app.use(
+  session({
+    secret: secretKey, // ここに生成した秘密鍵を設定
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // HTTPSを使用しないローカル環境ではfalseに設定
+  })
+);
 
 // EJSのビューエンジンを設定
 app.set("view engine", "ejs");
@@ -36,16 +50,40 @@ app.get("/asari", (req, res) => {
 });
 
 app.get("/area_input", (req, res) => {
-  res.render("area_input.ejs");
+  if (!req.session.userName) {
+    req.session.redirectAfterLogin = "area_input";
+    res.render("UserName_input.ejs");
+  } else {
+    res.render("area_input.ejs");
+  }
+});
+
+app.get("/hoko_input", (req, res) => {
+  if (!req.session.userName) {
+    req.session.redirectAfterLogin = "hoko_input";
+    res.render("UserName_input.ejs");
+  } else {
+    res.render("hoko_input.ejs");
+  }
 });
 app.get("/yagura_input", (req, res) => {
   res.render("yagura_input.ejs");
 });
-app.get("/hoko_input", (req, res) => {
-  res.render("hoko_input.ejs");
-});
 app.get("/asari_input", (req, res) => {
   res.render("asari_input.ejs");
+});
+
+app.use(express.urlencoded({ extended: true }));
+//リダイレクトしてくれるやつ
+app.post("/set_user_name", (req, res) => {
+  const userName = req.body.userName;
+  if (userName) {
+    req.session.userName = userName;
+    const redirectPath = req.session.redirectAfterLogin || "/";
+    res.redirect(redirectPath);
+  } else {
+    res.redirect("/area_input"); // エラーハンドリング
+  }
 });
 
 //一回エリアのバンカラから名前入力画面に移動できるようにする
